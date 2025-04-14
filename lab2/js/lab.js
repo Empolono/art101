d092e9aa86871aa444573b55b9d25e3b29130fe8
Empolono <demaggio11@gmail.com>
2025-04-14 14:46:39 -0700

    Lab02

--- lab2/js/lab.js
+++ lab2/js/lab.js
@@ -1,21 +1,17 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// Lab.js - HTML & CSS Starter
+// Author: Nick DeMaggio
+// Date: 4/13/2025
 
-// Constants
-
-// Functions
-
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
+function send_message(param1, param2) {
+  return `
+    <h1>Hello World</h1>
+    <p>I have already had a few classes with Julie and i think they are a great partner. This is because we both procrastonate but we always follow through. :)</p>
+  `;
 }
-
 function main() {
   console.log("Main function started.");
-  // the code that makes everything happen
+  const message = send_message();
+  document.getElementById("output").innerHTML = message;
 }
 
-// let's get this party started
 main();
