4d03497126221f23d831935ba172e08ba21ecb2a
Empolono <demaggio11@gmail.com>
2025-04-21 16:25:05 -0700

    lab 4

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,21 +1,42 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - BlackJack pseudocode
+// Author: Nick DeMaggio <ndemaggi>
+// Date: 4/21/2024
 
-// Constants
+//START
+//Initialize deck
+//Shuffle deck
+//Deal two cards to the player.
+//Deal two cards to the dealer.
+//Display the player's hand.
+//Display one dealer card (hide the other)
+//WHILE player wants to hit AND player total < 21
+    //Deal one card to the player.
+    //Display the updated hand
+//IF player total > 21
+    //Display "Player busts – Dealer wins
+    //END GAME
+//Reveal the dealer's hidden card.
+//WHILE dealer total < 17
+    //Deal one card to the dealer.
+    //Display updated dealer hand
+//IF dealer total > 21
+    //Display "Dealer busts – Player wins"
+//ELSE
+    //Compare the player's total to the dealer's total.
+    //IF player total > dealer total
+        //Display "Player wins"
+    //ELSE IF player total < dealer total
+        //Display "Dealer wins"
+    //ELSE
+        //Display "Push – It's a tie"
+//END GAME
 
-// Functions
-
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
 function myFunction(param1, param2) {
-  // some code here
-  // return results;
+
 }
 
 function main() {
   console.log("Main function started.");
-  // the code that makes everything happen
 }
 
-// let's get this party started
 main();
